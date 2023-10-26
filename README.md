6866371c89246d9e50fdffd1d54802e59557182c
jack <1286783182@qq.com>
2023-10-26 13:39:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# vite-plugin-csvg #
+
+## Vite Plugin for svg in CSS/Less/SCSS ##
+
+transform `url(**/*.svg)` to `url('data:image/svg+xml;utf8,<svg>...</svg>')`
+
+### eg ###
+
+```css
+1. single imported svg
+// index.css before transform
+.some-class{
+    background: url(src/asset/a.svg) // support alias eg. url(@/asset/a.svg)
+}
+// after 
+.some-class{
+    background: url('data:image/svg+xml;utf8,<svg>...</svg>')
+}
+2. imported svg used many times
+// before
+#root{
+    --svg-a: url(src/asset/a.svg)
+}
+.a-class{
+    background: var(--svg-a)
+}
+.b-class{
+    background: var(--svg-a)
+}
+// after
+body{
+    --svg-a: url('data:image/svg+xml;utf8,<svg>...</svg>')
+}
+.a-class{
+    background: var(--svg-a)
+}
+.b-class{
+    background: var(--svg-a)
+}
+```
+
+### params ###
+
+#### 1. exclude?: `FilterPattern` ####
+
+defualt: `undefined`
+
+#### 2. include?: `FilterPattern` ####
+
+defualt: `[/\.s?css/, /\.less/]`
+
+#### 3. inlineLimit?: `number` ####
+
+default: `10` (kb)
---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import { createFilter, FilterPattern } from "@rollup/pluginutils";
+import fs from "fs";
+import path from "path";
+import type { Alias, Plugin, ResolvedConfig } from "vite";
+
+/**
+ * @param inlineLimit (kb)
+ */
+export interface VitePluginCssSvgOptions {
+  exclude?: FilterPattern;
+  include?: FilterPattern;
+  inlineLimit?: number;
+}
+
+export default function CssSvgInlinePlugin({
+  include = [/\.s?css/, /\.less/],
+  exclude,
+  inlineLimit = 10,
+}: VitePluginCssSvgOptions = {}): Plugin {
+  const filter = createFilter(include, exclude);
+  const postfixRE = /[?#].*$/s;
+  let config: ResolvedConfig;
+
+  return {
+    name: "vite-plugin-csvg",
+    enforce: "pre", // to override `vite:asset`'s behavior
+    configResolved(resolvedConfig) {
+      // 记录最终配置
+      config = resolvedConfig;
+    },
+    async load(id) {
+      if (!filter(id)) return;
+      const filePath = id.replace(postfixRE, "");
+      const code = await fs.promises.readFile(filePath, "utf8");
+      const replaceMatch = code.match(/url\("?.*\.svg"?\)/g);
+      if (replaceMatch) {
+        let newCode = code;
+        for (let index = 0; index < replaceMatch.length; index++) {
+          const replaceMatchUrl = replaceMatch[index];
+
+          const svgUrlMatch = replaceMatchUrl.match(/(?<=(url\("?)).*\.svg/g);
+          if (!svgUrlMatch) continue;
+          let svgUrl = svgUrlMatch[0].replace('"', "");
+          const svgUrlArr = svgUrl.split(/\//);
+          const maybeAlias = svgUrlArr[0];
+          const alias = config.resolve.alias;
+          let match: Alias | undefined = undefined;
+          if (alias instanceof Array) {
+            const result = alias.find((item) => {
+              return item.find === maybeAlias;
+            });
+            if (result?.replacement) {
+              svgUrl = svgUrlArr
+                .map((item, index) => {
+                  if (index === 0) {
+                    return result.replacement;
+                  }
+                  return item;
+                })
+                .join("/");
+              match = result;
+            }
+          }
+
+          let svgData = replaceMatchUrl;
+          try {
+            const svg = fs.readFileSync(
+              match ? svgUrl : path.join(__dirname, svgUrl),
+              "utf-8"
+            );
+            if (svg.length < 1024 * inlineLimit) {
+              svgData = svg;
+              newCode = newCode.replace(
+                replaceMatchUrl,
+                `url('data:image/svg+xml;utf8,${encodeURIComponent(
+                  svgData.replace(/\t|\n|\v|\r|\f/g, "")
+                )}')`
+              );
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        return {
+          code: newCode,
+          map: null,
+        };
+      }
+    },
+  };
+}
